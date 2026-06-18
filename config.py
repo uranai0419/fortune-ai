f3f9a0ce5b4951edf7d06363b1a146e46a761a62
uranai0419 <uranai.0419@gmail.com>
2026-06-18 20:53:45 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # LINEチャネルアクセストークン
-ACCESS_TOKEN = "ここにLINEチャネルアクセストークン"
+ACCESS_TOKEN = "gegjRVo/b1p7oAruI3Rg89Jgv6G0m5v/Jvn0OsEDRosBvG+A0gWGBetO8k/jC/5HG99h8wKYcxY/0I8rfXKTi3YifnGZb1uZZUGf7zEugCXlqBda3IAZ1+6MW23InxqAv22+6+SB0gNvfkEXgMrq7wdB04t89/1O/w1cDnyilFU="
 
 # Gemini APIキー
-GEMINI_API_KEY = "ここにGemini APIキー"
+GEMINI_API_KEY = "AQ.Ab8RN6L9tbc-wDWsKVPxhHjg-Jmo6_aIm3dQLaNWhpyxKmQAhA"
 
 MODEL_NAME = "gemini-2.5-flash"
